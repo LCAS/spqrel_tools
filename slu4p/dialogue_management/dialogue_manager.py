f19a010ebc08fa2eef1c1b3d8fa42d41cde2a3cc
Andrea Vanzo <andrea.vanzo1@gmail.com>
2017-07-18 17:24:11 +0200

    minor fixes on the dialogue manager: now the asr is stopped at the end of the dialogue

--- slu4p/dialogue_management/dialogue_manager.py
+++ slu4p/dialogue_management/dialogue_manager.py
@@ -59,6 +59,8 @@ class DialogueManager(EventAbstractClass):
         to_send = ' '.join(splitted)
         if 'start' in splitted:
             self.memory.raiseEvent("ASR_enable", 1)
+        if 'stop' in splitted:
+            self.memory.raiseEvent("ASR_enable", 0)
         if 'missingdrink' in splitted:
             customer = self.cocktail_data.get(splitted[1], None)['customer']
             drink = self.cocktail_data[splitted[1]]['drink']
@@ -117,7 +119,7 @@ class DialogueManager(EventAbstractClass):
                 print "[" + self.inst.__class__.__name__ + "] Robot says: " + reply
                 self.memory.raiseEvent("Veply", reply)
             elif '[STOP]' in submessage:
-                self.memory.raiseEvent("ASR_enable", 1)
+                self.memory.raiseEvent("ASR_enable", 0)
             else:
                 print submessage
 

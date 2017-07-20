ba405dee963ff83602c799cb85ecdb48f21732f0
sergimolina <smolinamellado@lincoln.ac.uk>
2017-07-20 17:48:09 +0100

    -

--- actions/persondetected.py
+++ actions/persondetected.py
@@ -26,7 +26,6 @@ def rhMonitorThread (memory_service):
         plist = memory_service.getData("PeoplePerception/PeopleList")
         v = 'false'
         try:
-            print len(plist)
             if (len(plist)>0):
                 v = 'true'
         except:

67712b4be4d57e941a789a3f97051e876b399b26
sergimolina <smolinamellado@lincoln.ac.uk>
2017-07-20 17:31:49 +0100

    new action

--- actions/persondetected.py
+++ actions/persondetected.py
@@ -2,47 +2,77 @@
 # -*- encoding: UTF-8 -*-
 
 # DOCUMENTATION
-#http://doc.aldebaran.com/2-4/naoqi/peopleperception/alpeopleperception-api.html#alpeopleperception-api
+# http://doc.aldebaran.com/2-5/naoqi/peopleperception/alengagementzones-api.html#alengagementzones-api
 
 import qi
 import argparse
 import sys
 import os
 import time
+import threading
+import math
 
 from naoqi import ALProxy
 
 import conditions
 from conditions import set_condition
 
-def person_callback(data):
-    print "Person detected!"
-    print "ID: ",data
-    set_condition(memory_service,'persondetected','true')
-    time.sleep(1)
-    set_condition(memory_service,'persondetected','false')
+def rhMonitorThread (memory_service):
+    global last_personid
+    t = threading.currentThread()
+    print "personhere thread started"
+    personid = 0
+    while getattr(t, "do_run", True):
+        plist = memory_service.getData("PeoplePerception/PeopleList")
+        
+        personid = 0
+        if (plist!=None and len(plist)>0):
+            personid = plist[0]
+
+        v = 'false'
+        try:
+            pdist = memory_service.getData(pmemkey_dist)
+            #print "personhere:: distance ",pdist
+            if (personid>0):
+                v = 'true'
+        except:
+            v = 'false'
+
+        set_condition(memory_service,'persondetected',v)
+
+        time.sleep(0.5)
+    print "persondetected thread quit"
 
 
-def init(session,app):
+
+def init(session):
     global memory_service
-    
-    print "Person detector init"
+    global monitorThread
+
+    print "Person here init"
 
     #Starting services
     memory_service  = session.service("ALMemory")
+    zones_service = session.service("ALEngagementZones")
     people_service = session.service("ALPeoplePerception")
-
-    # PARAMETERS
+    people_service.resetPopulation()
     
-    try:
-        peopleDetection = memory_service.subscriber("PeoplePerception/JustArrived")
-        idAnyDetection = peopleDetection.signal.connect(person_callback)   
-    except RuntimeError:
-        print "Cannot find ALPeoplePerception service. Condition personhere not available"
+    #waving_service = session.service("ALWavingDetection")
+    #movement_service = session.service("ALMovementDetection")
+
+    print "Creating the thread"
+
+    #create a thead that monitors directly the signal
+    monitorThread = threading.Thread(target = rhMonitorThread, args = (memory_service,))
+    monitorThread.start()
+
 
 
 def quit():
+    global monitorThread
     print "Person here quit"
+    monitorThread.do_run = False 
+
 
 
 def main():
@@ -60,7 +90,7 @@ def main():
     try:
         connection_url = "tcp://" + pip + ":" + str(pport)
         print "Connecting to ",    connection_url
-        app = qi.Application(["PersonDetected", "--qi-url=" + connection_url ])
+        app = qi.Application(["PersonHere", "--qi-url=" + connection_url ])
     except RuntimeError:
         print ("Can't connect to Naoqi at ip \"" + pip + "\" on port " + str(pport) +".\n"
                "Please check your script arguments. Run with -h option for help.")

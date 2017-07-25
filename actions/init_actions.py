45ed7d67c97a7a4b3e85d0b97eb1b21a31c7e835
sergimolina <smolinamellado@lincoln.ac.uk>
2017-07-25 15:37:51 +0900

    added navigateto

--- actions/init_actions.py
+++ actions/init_actions.py
@@ -17,7 +17,7 @@ from action_base import *
 import dooropen, obstaclehere, screentouched
 import say, vsay, wait, waitfor, goto, turn, lookfor, dialogue, dialoguestart, dialoguestop, asrenable
 import posture, personhere, headpose, followuntil, movementdetected, webpage, personbehind, persondetected, speechbtn
-import execplan, saveposition, soundtrack
+import execplan, saveposition, soundtrack, navigateto
 
 def init(session):
     screentouched.init(session)

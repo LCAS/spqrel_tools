bf92e6a9ef7b3efcc195c676893d3e4480a19a4e
Luca Iocchi <iocchi@dis.uniroma1.it>
2018-06-14 12:38:12 +0200

    config html, added team logo

--- scripts/config_spqrel_app_on_pepper.bash
+++ scripts/config_spqrel_app_on_pepper.bash
@@ -2,12 +2,20 @@
 
 PEPPER_SPQREL_DIR=${HOME}/spqrel
 
+# create app folder
 cd ~/.local/share/PackageManager/apps/
 mkdir -p spqrel/html/
 cd spqrel/html/
-echo "<html><body><h1>Ciao</h1></body></html>" > index.html
+# create home page with logo
+echo "<html><body><br><br><br><center><img src=\"img/logo.gif\" width=800></center></body></html>" > index.html
+mkdir img
+cd img
+cp ${PEPPER_SPQREL_DIR}/spqrel_launch/worktree/spqrel_tools/html/img/logo.gif .
+# setting links to spqrel html app 
 cd ${PEPPER_SPQREL_DIR}
 ln -s ~/.local/share/PackageManager/apps/spqrel/html/ html
 cd html
 ln -s ${PEPPER_SPQREL_DIR}/spqrel_launch/worktree/spqrel_tools/modim_actions robocup2018
+
+
 

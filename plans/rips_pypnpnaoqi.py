e06f12cedc1e4eaeb8216f02af76aa51bd151ac4
Marc Hanheide <marc@hanheide.net>
2018-06-17 18:48:18 +0100

    first rips version

--- plans/rips_pypnpnaoqi.py
+++ plans/rips_pypnpnaoqi.py
@@ -7,31 +7,38 @@ except:
     print "Please set PNP_HOME environment variable to PetriNetPlans folder."
     sys.exit(1)
 
-import pnp_cmd_naoqi
 from pnp_cmd_naoqi import *
 
 p = PNPCmd()
 
 p.begin()
 
+p.exec_action('taskstep', 'waiting')
 p.exec_action('modiminit', 'rips')
-#p.exec_action('interact', 'ready')
-#while (not p.get_condition('dooropen')):
-#    time.sleep(1)
-    
-#p.exec_action('interact', 'rips')
-#p.exec_action('enter', '30_0_0_4_true')
+p.exec_action('interact', 'ready')
+while (not p.get_condition('dooropen')):
+    time.sleep(1)
 
-#p.exec_action('goto', 'rips', interrupt='obstaclehere', recovery='say_hello; waitfor_not_obstaclehere; restart_action')
+p.exec_action('interact', 'rips')
+p.exec_action('taskstep', 'Entering')
+
+p.exec_action('enter', '30_0_0_4_true')
+
+p.exec_action('taskstep', 'going_to_inspection_point')
+p.exec_action(
+    'navigateto', 'wp5',
+    interrupt='obstaclehere',
+    recovery='say_hello; waitfor_not_obstaclehere; restart_action')
 
 p.exec_action('interactq', 'inspectme')
-print p.get_condition(u'continue')
 print p.get_condition('continue')
 if (p.get_condition('continue')):
     print "I'm done"
 p.exec_action('say', 'goodbye')
 
-#p.exec_action('goto', 'door', interrupt='aborted', recovery='restart_action')
+p.exec_action(
+    'navigateto',
+    'wp13', interrupt='aborted', recovery='restart_action')
 
 p.end()
 

9c0bda82b02340fac4162071ffcf311d5bef4ed2
Sergi Molina <sergimolina91@gmail.com>
2018-06-18 15:17:59 -0400

    updated contention plan cocktail

--- plans/cocktail_party_pypnpnaoqi_nodetections.py
+++ plans/cocktail_party_pypnpnaoqi_nodetections.py
@@ -10,13 +10,26 @@ except:
 import pnp_cmd_naoqi
 from pnp_cmd_naoqi import *
 
+
+FAKE = True
+
 p = PNPCmd()
 
 p.begin()
+p.exec_action('setpose', '11.5_8.6_0')
 
 p.exec_action('taskstep', 'waiting')
 p.exec_action('modiminit', 'cocktailparty')
 p.exec_action('interact', 'ready')
+
+
+
+###  delete that
+if FAKE:
+    print 'FAKE'
+    p.set_condition('dooropen', "true")
+###
+
 
 while (not p.get_condition('dooropen')):
     time.sleep(1)
@@ -28,60 +41,59 @@ p.exec_action('enter', '30_0_0_4_true')
 p.exec_action('taskstep', 'going_to_party_room')
 p.exec_action('navigateto', 'wp8', interrupt='aborted', recovery='restart_action')
 
+p.exec_action('turn','-135_ABS', interrupt='timeout_30')
 
 # start looking for orders
-p.exec_action('say', 'I_am_ready_to_take_the_orders')
+p.exec_action('say', 'I_am_ready_to_take_the_orders', interrupt='timeout_5')
 
-p.exec_action('headpose','0_-10')
+p.exec_action('headpose','0_-20',interrupt='timeout_5')
 
 
+# take order 1
+p.exec_action('say','Please_can_someone_come_here_for_the_other?',interrupt='timeout_5')
 
-#order 1
-p.exec_action('say','Please_can_you_come_here?')
 while (not p.get_condition('personhere')):
-     time.sleep(0.5)
-
-p.exec_action("say", "hello,_whats_your_name?")
-
-
-p.exec_action("say", "what_drink_do_you_want?")
+    if FAKE:
+        print 'FAKE'
+        p.set_condition('personhere', "true")
+    time.sleep(0.5)
 
 
-p.exec_action("say", "thanks")
+p.exec_action("say", "What's_your_name?",interrupt='timeout_5')
+
+p.exec_action("asr", "name")
+
+name_response_1 = p.memory_service.getData("asrresponse").replace(" ", "_")
+print "name response", name_response_1
+
+if name_response_1 != "":
+    p.exec_action("say", "Is_"+ name_response_1 +"_your_name?",interrupt='timeout_5')
+
+    p.exec_action("asr", "confirm")
+
+    confirm_response = p.memory_service.getData("asrresponse").replace(" ", "_")
+    if confirm_response != "" and confirm_response == "yes":
+        p.exec_action("say", "What_drink_do_you_want?",interrupt='timeout_5')
+
+        p.exec_action("asr", "drink")
+
+        drink_response_1 = p.memory_service.getData("asrresponse").replace(" ", "_")
+
+        if drink_response_1 != "":
+            p.exec_action("say", "I_will_bring_you_a_" + drink_response_1,interrupt='timeout_5')
+    else:
+        p.exec_action("say", "I_am_not_sure_I_understood_,_can_you_repeat?",interrupt='timeout_5')
+
+
+p.exec_action("say","Can_you_look_at_me_for_some_seconds?",interrupt='timeout_5')
+p.exec_action('persondescription', 'order1',interrupt='timeout_10')
+p.exec_action("say", "thanks",interrupt='timeout_5')
 
 
 
-#order 2
-p.exec_action('say','Please_can_you_come_here?')
-while (not p.get_condition('personhere')):
-     time.sleep(0.5)
+# take order 2
 
-p.exec_action("say", "hello,_whats_your_name?")
-
-
-
-p.exec_action("say", "what_drink_do_you_want?")
-
-
-p.exec_action("say", "thanks")
-
-
-#order 3
-p.exec_action('say','Please_can_you_come_here?')
-while (not p.get_condition('personhere')):
-     time.sleep(0.5)
-
-p.exec_action("say", "hello,_whats_your_name?")
-
-
-
-p.exec_action("say", "what_drink_do_you_want?")
-
-
-p.exec_action("say","can_you_look_at_me_for_some_seconds")
-
-
-p.exec_action("say", "thanks")
+# take order 3
 
 
 
@@ -89,9 +101,7 @@ p.exec_action("say", "thanks")
 
 p.exec_action('goto', 'bar', interrupt='aborted', recovery='skip_action')
 
-#say order1
-
-name = "John"
+# say order1
 age = memory_service.insertData("Actions/persondescription/order1/age")
 gender = memory_service.insertData("Actions/persondescription/order1/gender")
 haircolor = memory_service.insertData("Actions/persondescription/order1/hair")
@@ -100,129 +110,35 @@ glasses = memory_service.insertData("Actions/persondescription/order1/glasses")
 if gender == "male":
     feat = memory_service.insertData("Actions/persondescription/order1/beard")
     if feat == "yes":
-        extra = "with beard"
+        extra = "with_beard"
     else:
-        extra = "no beard"
+        extra = "no_beard"
 
 else
     feat = memory_service.insertData("Actions/persondescription/order1/makeup")
     if feat == "yes":
-        extra = "with makeup"
+        extra = "with_makeup"
     else:
-        extra = "no makeup"
+        extra = "no_makeup"
+
+if glasses == "yes":
+    isglasses = "glasses"
+else:
+    isglasses = "no_glasses"
 
 
-order_sentence = "Order 1"
-say()
+order1_sentence = name_response_1+",_who_is_a_"+gender+",_around"+age+"_years_old,_with_"+haircolor+"_hair,_"+extra+",_and_with_"+isglasses+"_wants_a_"+drink_response_1
+p.exec_action('say','order1_sentence',interrupt='timeout_5')
 
 # example: 'John', who is a 'male/female', around '25' years old, with 'black' hair, 'nobeard/nomakeup', and 'noglasses' wants a 'coke'.
 
 
+# say order2
+
+# say order 3
 
 
 
-
-
-
-
-
-
-
-
-
-# Search for the person sitting
-p.exec_action('lookfor','personsitting',interrupt='timeout_20')
-
-if p.get_condition('personsitting'):
-    xsitting = p.memory_service.getData("Condition/personsitting/robot_coordinates_x")
-    ysitting = p.memory_service.getData("Condition/personsitting/robot_coordinates_y")
-
-    print "SITTING X: ",xsitting
-    print "SITTING Y: ",ysitting
-
-    p.exec_action("navigateto_naoqi",str(xsitting)+'_'+str(ysitting))
-
-
-    # TODO 
-    p.exec_action("say", "whats_your_name?")
-
-    # ASR function 
-    p.exec_action("say",'^person1name')
-
-
-    p.exec_action("say", "what_drink?")
-
-
-
-	p.exec_action('persondescription', 'order1')
-
-
-p.exec_action('headpose', '0_0')
-
-
-#p.exec_action("iswaving","0.2_0.5_wavingdetected")
-
-#xwaving = p.memory_service.getData('Actions/wavingdetected/wavingpersonx')
-#ywaving = p,memory_service.getData('Actions/wavingdetected/wavingpersony')
-
-#print "WAVING X: ",xwaving
-#print "WAVING Y: ",ywaving
-
-#p.exec_action("navigateto_naoqi",str(xwaving)+'_'+str(ywaving))
-
-
-
-
-
-
-
-
-
-
-
-
-
-p.exec_action('say', 'I_will_look_for_someone_calling_me')
-
-#while (not p.get_condition('personhere')):
-#    time.sleep(1)
-
-p.exec_action('lookfor', 'personhere')
-#print p.memory_service.getData('Actions/personhere/PersonAngleYaw')
-#print p.memory_service.getData('Actions/personhere/PersonAngleTurn')
-
-p.exec_action('turn', '^Actions/personhere/PersonAngleTurn')
-
-p.exec_action('say', 'hello')
-
-p.exec_action('goto', 'bar', interrupt='aborted', recovery='skip_action')
-
-p.exec_action('goto', 'door', interrupt='aborted', recovery='skip_action')
-
-#p.exec_action('interact', 'comehere')
-
-# p.exec_action('say', 'hello')
-
-# while (not p.get_condition('screentouched')):
-#     time.sleep(1)
-
-# p.exec_action('say', 'starting')
-
-# i=0
-
-#remove comment if you want the robot to move
-#p.exec_action('turn', '-90', interrupt='screentouched', recovery='skip_action')
-
-# while (i<5 and not p.get_condition('screentouched')):
-    
-#     y = -40 + 20*i
-#     p.exec_action('headpose', '%d_20' %y)
-#     time.sleep(0.5)
-#     i += 1
-
-# p.exec_action('headpose', '0_-10')
-
-# p.exec_action('say', 'goodbye')
 
 p.end()
 

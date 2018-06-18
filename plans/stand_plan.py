318a98fc33fe9d235edfe3738dd1d78fc3ba62b6
Sergi Molina <sergimolina91@gmail.com>
2018-06-18 11:23:59 -0400

    posture plans

---
+++ plans/stand_plan.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+
+try:
+    sys.path.insert(0, os.getenv('PNP_HOME')+'/PNPnaoqi/py')
+except:
+    print "Please set PNP_HOME environment variable to PetriNetPlans folder."
+    sys.exit(1)
+
+import pnp_cmd_naoqi
+from pnp_cmd_naoqi import *
+
+p = PNPCmd()
+
+p.begin()
+
+p.exec_action("posture","Stand")
+
+p.end()

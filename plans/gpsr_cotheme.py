b0f92c62bbcd9ecca36a2d77ad6447455303340f
Sergi Molina <sergimolina91@gmail.com>
2018-06-19 10:39:59 -0400

    gpsr

---
+++ plans/gpsr_cotheme.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+try:
+    sys.path.insert(0, os.getenv('PNP_HOME')+'/PNPnaoqi/py')
+except:
+    print "Please set PNP_HOME environment variable to PetriNetPlans folder."
+    sys.exit(1)
+
+import pnp_cmd_naoqi
+from pnp_cmd_naoqi import *
+
+
+def gpsr_cotheme(p, req):
+    try:
+        return p.exec_action('say', 'cotheme')
+    except:
+        return p.exec_action('say', "sorry_I_can't_do_this_right_now")
+
+if __name__ == '__main__':
+    p = PNPCmd()
+
+    p.begin()
+
+    gpsr_cotheme(p, {
+        'object': {
+            'location': 'kitchen'
+        }
+    })
+
+    p.end()
---
+++ plans/gpsr_tell.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+try:
+    sys.path.insert(0, os.getenv('PNP_HOME')+'/PNPnaoqi/py')
+except:
+    print "Please set PNP_HOME environment variable to PetriNetPlans folder."
+    sys.exit(1)
+
+import pnp_cmd_naoqi
+from pnp_cmd_naoqi import *
+
+
+def gpsr_tell(p, req):
+    try:
+        return p.exec_action('say', 'motion')
+    except:
+        return p.exec_action('say', "sorry_I_can't_do_this_right_now")
+
+if __name__ == '__main__':
+    p = PNPCmd()
+
+    p.begin()
+
+    gpsr_tell(p, {
+        'whattosay': {
+            'text': 'test text'
+        }
+    })
+
+    p.end()

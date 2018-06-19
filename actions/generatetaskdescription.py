8d4bf11ed417cb9ef207e657cccc7ff47be92a0f
francescodelduchetto <francescodelduchetto@gmail.com>
2018-06-19 11:55:10 -0400

    description of commands

--- actions/generatetaskdescription.py
+++ actions/generatetaskdescription.py
@@ -4,12 +4,22 @@ import argparse
 import sys
 import time
 import threading
+import random
 import re
+import pprint as pp
 
 import action_base
 from action_base import *
 
 actionName = "generatetaskdescription"
+
+SEMANTIC_INFO_MEM = "/semantic_info"
+
+tasks_definition = []
+objects = []
+names = []
+locations = []
+questions = []
 
 
 def actionThread_exec (params):
@@ -19,21 +29,24 @@ def actionThread_exec (params):
 
     print "Action "+actionName+" started with params "+params
 
-    tasks = eval(memory_service.getData("interpreted_tasks"))
-    ttc = memory_service.getData("tasks_to_confirm")
-    asr_string = memory_service.getData("command_sentence")
+    task = eval(memory_service.getData("CurrentTaskInterpretation"))
 
-    current_task_i = len(tasks) - ttc
-    current_task = tasks[current_task_i]
-    print "Confirming:", str(current_task)
+    # current_task_i = len(tasks) - ttc
+    # current_task = tasks[current_task_i]
+    print "Confirming:",
+    pp.pprint(task)
 
-    to_say = getToSay(current_task, memory_service)
+    print "command:", str(params)
+
+    to_say = getToSay(task)
 
     print "Asking:", to_say
 
-    memory_service.insertData("tasks_to_confirm", ttc-1)
+    #memory_service.insertData("tasks_to_confirm", ttc-1)
 
     memory_service.insertData("task_description", to_say)
+
+    # time.sleep(2)
 
     action_success(actionName,params)
 
@@ -47,162 +60,202 @@ def to_memory_key(filler, memory_service):
         return ''
     return memory_key
 
-def getToSay(current_task, memory_service):
-    action = ""
-    to_say = ""
-    dirty_frame = current_task.keys()[0]
-    if "-" in dirty_frame:
-        frame = dirty_frame.split("-")[1]
-    else: frame = dirty_frame
-    args = {}
-    for arg in current_task[dirty_frame]:
-        mods = ""
-        for k in arg.keys():
-            # put the mods togheter with the argvalue
-            if k == "mods":
-                mods = " ".join(arg["mods"]) + " "
-            else:
-                argk = k
-                argv = arg[k]
-        args.update({argk: mods + argv})
-    if len(args) > 0:
-        if frame == 'motion' or frame == 'arriving':
-            to_say = to_say + "I understood that I need to go. "
-            for arg in args:
-                if 'goal' in arg:
-                    filler = args[arg]
-                    location = to_memory_key(filler, memory_service)
-                    if len(location) == 0:
-                        to_say = to_say + "I'm sorry, I understood the word " + filler + ", but I failed to ground it. "
-            if len(location) > 0:
-                to_say = to_say + "I understood that the location is " + filler + ". "
-                action = action + 'navigateto_' + location + ';'
-            #memory_service.raiseEvent("Veply", to_say)
-        elif frame == 'cotheme':
-            to_say = to_say + "I understood that I need to follow. "
-            for arg in args:
-                if 'cotheme' in arg:
-                    filler = args[arg]
-                    if "me" in filler:
-                        filler = "you"
-                    to_say = to_say + "I understood that I need to follow " + filler + ". I will do it until I receive the stop command. "
-            if 'you' in filler:
-                action = action + ' vsay_followyou; headpose_0_-20; followuntil_screentouched; '
-            else:
-                to_say = to_say + "I don't know how to identify Maria. Please, go on! "
-            #memory_service.raiseEvent("Veply", to_say)
-        elif frame == 'bringing' or frame == 'giving':
-            theme_filler = ''
-            theme_memory_key = ''
-            goal_filler = ''
-            goal_memory_key = ''
-            to_say = to_say + "I understood that I need to bring. "
-            for arg in args:
-                if 'theme' in arg:
-                    theme_filler = args[arg]
-                    to_say = to_say + "I understood that the object is " + theme_filler + ". "
-                    theme_memory_key = to_memory_key(theme_filler, memory_service)
-                    if len(theme_memory_key) == 0:
-                        to_say = to_say + "I'm sorry, I am not able to ground " + theme_filler + ". "
-                if ('beneficiary' in arg) or ('recipient' in arg):
-                    filler = args[arg]
-                    if filler == 'me':
-                        filler = 'you'
-                    to_say = to_say + "I understood that I have to bring it to " + filler + ". "
-                if 'goal' in arg:
-                    goal_filler = args[arg]
-                    goal_memory_key = to_memory_key(goal_filler, memory_service)
-                    to_say = to_say + "I understood that the final position of the object will be " + goal_filler + ". "
-                    if len(goal_memory_key) == 0:
-                        to_say = to_say + "Sorry, I'm not allowed to bring the " + goal_filler + ". "
-                if 'source' in arg:
-                    source_filler = args[arg]
-                    to_say = to_say + "I understood that the initial position of the object is " + source_filler + ". "
-            if len(theme_memory_key) > 0:
-                action = action + ' navigateto_' + theme_memory_key + '; vsay_cannottake; wait_10; '
-            else:
-                to_say = to_say + "I'm sorry, but I don't have the object " + theme_filler + " in my semantic map. "
-            if len(goal_memory_key) > 0:
-                action = action + ' navigateto_' + goal_memory_key + ';'
-            else:
-                to_say = to_say + "I'm sorry, but I don't have the goal of the action in my semantic map. "
-            #memory_service.raiseEvent("Veply", to_say)
-        elif frame == 'taking' or frame == 'manipulation':
-            theme_filler = ''
-            theme_memory_key = ''
-            goal_filler = ''
-            goal_memory_key = ''
-            to_say = to_say + "I understood that I need to bring. "
-            for arg in args:
-                if 'theme' in arg:
-                    theme_filler = args[arg]
-                    to_say = to_say + "I understood that the object is " + theme_filler + ". "
-                    theme_memory_key = to_memory_key(theme_filler, memory_service)
-                    if len(theme_memory_key) == 0:
-                        to_say = to_say + "I'm sorry, I am not able to ground " + theme_filler + ". "
-                if 'source' in arg:
-                    source_filler = args[arg]
-                    to_say = to_say + "I understood that the initial position of the object is " + source_filler + ". "
-            if len(theme_memory_key) > 0:
-                action = action + ' navigateto_' + theme_memory_key + '; vsay_cannottake; wait_10; '
-            else:
-                to_say = to_say + "I'm sorry, but I don't have the object " + theme_filler + " in my semantic map. "
-            #memory_service.raiseEvent("Veply", to_say)
-        elif frame == 'locating':
-            ground_filler = ''
-            ground_memory_key = ''
-            phenomenon_filler = ''
-            phenomenon_memory_key = ''
-            #to_say = to_say + "I understood that I need to find. "
-            for arg in args:
-                if ('ground' in arg) or ('entity' in arg):   # kitchen
-                    ground_filler = args[arg]
-                    ground_memory_key = to_memory_key(ground_filler, memory_service)
-                    to_say = to_say + "I understood the entity to find is in " + ground_filler + ". "
-                if 'phenomenon' in arg:
-                    phenomenon_filler = args[arg]
-                    phenomenon_memory_key = to_memory_key(phenomenon_filler, memory_service)
-                    to_say = to_say + "I understood that I have to look for " + phenomenon_filler + ". "
-            if len(phenomenon_memory_key) > 0:
-                to_say = to_say + "I'm going to look for " + phenomenon_filler
-                action = action + ' navigateto_' + phenomenon_filler + "; wait_10; vsay_notfound; wait_10;"
-            elif len(ground_memory_key) > 0:
-                to_say = to_say + "I'm going to look for " + ground_filler
-                action = action + ' navigateto_' + phenomenon_filler + "; wait_10; vsay_notfound; wait_10;"
-            else:
-                tmp_filler = ""
-                if phenomenon_filler != "" and ground_filler != "":
-                    tmp_filler = "neither " + phenomenon_filler + " nor " + ground_filler
-                elif phenomenon_filler != "":
-                    tmp_filler = phenomenon_filler
-                elif ground_filler != "":
-                    tmp_filler = ground_filler
-                if tmp_filler == "":
-                    to_say = to_say + "I'm sorry, I did not understand what I have to find."
-                else:
-                    to_say = to_say + "I'm sorry, I don't have " + tmp_filler + " in my semantic map. "
+def load_gpsr_xmls(memory_service):
+    global gpsr_tasks_definition, spr_tasks_definition, objects, names, locations, questions
+    gpsr_tasks_definition = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/gpsr_tasks_definition") )
+    spr_tasks_definition = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/spr_tasks_definition") )
+    objects = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/objects") )
+    names = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/names") )
+    locations = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/locations") )
+    questions = eval(memory_service.getData(SEMANTIC_INFO_MEM + "/questions") )
+    print "Semantic info loaded!"
 
-            #memory_service.raiseEvent("Veply", to_say)
+def resolveMultipleSpotted(task):
+    verb_index = task["index"]
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            lowest_index = 1000
+            lowest_attribute = None
+            # TODO gound it
+            if num_spotted > 1:
+                for spot in attribute["spotted"]:
+                    if spot["index"] < lowest_index and spot["index"] > verb_index:
+                        lowest_attribute = spot
+                        lowest_index = spot["index"]
+            if lowest_attribute is not None:
+                attribute["spotted"] = lowest_attribute
+    return task
+
+def describeTaking(task):
+    to_say = ""
+    verb = task["verb"]
+
+    obj = None
+    loc = None
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            if num_spotted > 0:
+                if "object" in attribute.keys():
+                    obj = attribute["spotted"]["text"]
+                elif "location" in attribute.keys():
+                    loc = attribute["spotted"]["text"]
+
+    print "obj:", obj, "loc:", loc
+    if obj is None and loc is None:
+        to_say = "I understood that I need to " + verb + "something but I didn't got what. Can you please repeat what I need to "+ verb +"?"
+    elif obj is not None and loc is not None:
+        to_say = "I understood that I need to " + verb + " the " + obj + " from the " + loc + ". I am going to take it!"
+    elif obj is not None:
+        to_say = "I understood that I need to " + verb + " the " + obj + ". I am going to take it!"
+    elif loc is not None:
+        to_say = "I understood that I need to " + verb + " somthing from the " + loc + ". Can you please repeat which object?"
+
+    return to_say
+
+def describePlacing(task):
+    verb = task["verb"]
+    to_say = ""
+
+    obj = None
+    loc = None
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            if num_spotted > 0:
+                if "object" in attribute.keys():
+                    obj = attribute["spotted"]["text"]
+                elif "location" in attribute.keys():
+                    loc = attribute["spotted"]["text"]
+
+    print "obj:", obj, "loc:", loc
+    if obj is None and loc is None:
+        to_say = "I understood that I need to " + verb + "something but I didn't got what. Can you please repeat what I need to "+ verb +"?"
+    elif obj is not None and loc is not None:
+        to_say = "I understood that I need to " + verb + " the " + obj + " on the " + loc + ". I am going to do it!"
+    elif obj is not None:
+        to_say = "I understood that I need to " + verb + " the " + obj + ", but I am not sure where. Can you please repeat the location?"
+    elif loc is not None:
+        to_say = "I understood that I need to " + verb + " the something on the "+ loc + ". Can you please repeat the object?"
+    return to_say
+
+def describeLocating(task):
+    verb = task["verb"]
+    to_say = ""
+
+    obj = None
+    loc = None
+    name = None
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            if num_spotted > 0:
+                if "object" in attribute.keys():
+                    obj = attribute["spotted"]["text"]
+                elif "location" in attribute.keys():
+                    loc = attribute["spotted"]["text"]
+                elif "name" in attribute.keys():
+                    name = attribute["spotted"]["text"]
+
+    print "obj:", obj, "loc:", loc
+    if obj is None and name is None:
+        to_say = "I understood that I need to " + verb + "something but I didn't got what. Can you please repeat?"
+    elif obj is not None:
+        to_say = "I understood that I need to " + verb + " the " + obj + ". I am going to do it!"
+    elif name is not None:
+        to_say = "I understood that I need to " + verb + " " + name + ". I am going to do it!"
+
+    return to_say
+
+def describeBringing(task):
+    verb = task["verb"]
+    to_say = ""
+
+    return to_say
+
+def describeMotion(task):
+    verb = task["verb"]
+    to_say = ""
+
+    loc = None
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            if num_spotted > 0:
+                if "location" in attribute.keys():
+                    loc = attribute["spotted"]["text"]
+
+    print "loc:", loc
+    if loc is None:
+        to_say = "I understood that I need to " + verb + " but I didn't got where. Can you please repeat?"
     else:
-        print "No args"
-        if frame == 'motion' or frame == 'arriving':
-            to_say = to_say + "I understood that I need to go, but I don't know where. "
-        elif frame == 'cotheme':
-            to_say = to_say + "I just understood that I need to follow someone. "
-        elif frame == 'bringing' or frame == 'giving':
-            to_say = to_say + "I understood that I need to bring, but I didn't get what. "
-        elif frame == 'taking' or frame == 'manipulation':
-            to_say = to_say + "I understood that I need to take, but I don't know what. "
-        elif frame == 'locating':
-            to_say = to_say + "I understood that I need to search, but I don't know what. "
-        #memory_service.raiseEvent("Veply", to_say)
-        print "action:", action
+        to_say = "I understood that I need to " + verb + " to " + loc + ". I am going there!"
+
+    return to_say
+
+def describeTell(task):
+    verb = task["verb"]
+    to_say = ""
+
+    wts = None
+    for attribute in task["requires"]:
+        if "spotted" in attribute.keys():
+            num_spotted = len(attribute["spotted"])
+            if num_spotted > 0:
+                if "whattosay" in attribute.keys():
+                    wts = attribute["spotted"]["text"]
+
+    print "wts:", wts
+    if wts is None:
+        to_say = "I understood that I need to " + verb + " something, but not what. Can you please repeat?"
+    else:
+        to_say = "I understood that I need to " + verb + wts + "!"
+
+    return to_say
+
+def describeAnswer(task):
+    verb = task["verb"]
+    to_say = ""
+
+    to_say = "I understood that I need to " + verb + "a question!"
+
+    return to_say
+
+
+#def describeTaking(task):
+#    verb = random.choice(task["possible_verbs"])
+
+
+
+def getToSay(current_task):
+    current_task = resolveMultipleSpotted(current_task)
+    pp.pprint(current_task)
+    to_say = ""
+    if current_task["task"] == "taking":
+        to_say = describeTaking(current_task)
+    elif current_task["task"] == "locating":
+        to_say = describeLocating(current_task)
+    elif current_task["task"] == "bringing":
+        to_say = describeBringing(current_task)
+    elif current_task["task"] == "motion":
+        to_say = describeMotion(current_task)
+    elif current_task["task"] == "tell":
+        to_say = describeTell(current_task)
+    elif current_task["task"] == "placing":
+        to_say = describePlacing(current_task)
+    elif current_task["task"] == "answer":
+        to_say = describeAnswer(current_task)
 
     return to_say
 
 def init(session):
     print actionName+" init"
     action_base.init(session, actionName, actionThread_exec)
+
+    memory_service = session.service("ALMemory")
+
+    load_gpsr_xmls(memory_service)
 
 def quit():
     print actionName+" quit"
--- plans/test_gpsr.py
+++ plans/test_gpsr.py
@@ -15,13 +15,13 @@ p = PNPCmd()
 
 p.begin()
 
-p.exec_action("say", "Ask_me_a_question!")
-
-# This blocks until we get a transcription from google (start language_understanding/google_client.py)
-p.exec_action("googleasr", "gpsr", interrupt="timeout_20")
+# p.exec_action("say", "Ask_me_a_question!")
+#
+# # This blocks until we get a transcription from google (start language_understanding/google_client.py)
+# p.exec_action("googleasr", "gpsr", interrupt="timeout_20")
 
 # get the google transcription
-googleasr_value = p.memory_service.getData("googleasrresponse")
+googleasr_value = p.memory_service.insertData("googleasrresponse", ["Pick up the fork from the chair and place it on the bookcase"])
 print "plan google response", googleasr_value
 
 # This blocks until we get the task description from the transcription (start language_understanding/language_understanding.py)
@@ -31,18 +31,27 @@ p.exec_action("understandcommand", "gpsr", interrupt="timeout_20")
 commands_inter = eval(p.memory_service.getData("CommandInterpretation"))
 print "plan commands interpretation", pp.pprint(commands_inter)
 
+#tell the number of actions
+p.exec_action("say", "I_understood_" + str(len(commands_inter)) + "_commands.")
+
 #TODO these need to be ordered in order to be executed in order
 for i, task in enumerate(commands_inter):
+    # print task
     print "Task", i, ": ", task["task"]
-
     print "\tParameters:"
     for req in task["requires"]:
         if "spotted" in req:
             for spotreq in req["spotted"]:
                 print "\t"*2, [k for k in req.keys() if k != "spotted"][0] +":", spotreq["text"]
 
+    # describe task
 
+    p.memory_service.insertData("CurrentTaskInterpretation", str(task))
+    p.exec_action("generatetaskdescription", str(i))
 
+    to_say = str(p.memory_service.getData("task_description"))
+
+    p.exec_action("say", to_say.replace(" ", "_"))
 
 
 p.end()
